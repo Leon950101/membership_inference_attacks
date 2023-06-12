b9b53be5a7d7da5b0e390056b6a8c971777c2fe9
Yang Li <yali00004@stud.uni-saarland.de>
2023-06-12 15:16:04 +0200

    d

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import torch
 import matplotlib.pyplot as plt
 import torchvision.models as models
 
-# For shadow.p
+## For shadow.p
 # DATA_PATH = '../pickle/cifar10/resnet34/shadow.p'
 
 # device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -23,7 +23,7 @@ import torchvision.models as models
 # for batch_idx, (img, label) in enumerate(dataloader):
 #     img = img.to(device)
 
-# For eval.p
+## For eval.p
 # DATA_PATH = '../pickle/cifar10/resnet34/eval.p'
 # # Change the DATA_PATH to your local pickle file path
 
@@ -45,31 +45,35 @@ import torchvision.models as models
 # for batch_idx, (img, label, isMemeber) in enumerate(dataloader):
 #     img = img.to(device)
 
-MODEL_PATH = '../models/resnet34_cifar10.pth'
-# Change the MODEL_PATH to your local model path
+## Model load
+# MODEL_PATH = '../models/resnet34_cifar10.pth'
+# # Change the MODEL_PATH to your local model path
 
-device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+# device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-target_model = models.resnet34(num_classes=10).to(device)
-# Change num_classes to 200 when you use the Tiny ImageNet dataset
+# target_model = models.resnet34(num_classes=10).to(device)
+# # Change num_classes to 200 when you use the Tiny ImageNet dataset
 
-state_dict = torch.load(MODEL_PATH, map_location=device)
-target_model.load_state_dict(state_dict['net'])
+# state_dict = torch.load(MODEL_PATH, map_location=device)
+# target_model.load_state_dict(state_dict['net'])
 
-print(target_model)
+# print(target_model)
 
+## Result save
+# DATA_PATH = '../pickle/cifar10/resnet34/test.p'
 
-DATA_PATH = '../pickle/cifar10/resnet34/test.p'
+# with open(DATA_PATH, "rb") as f:
+#     dataset = pickle.load(f)
 
-with open(DATA_PATH, "rb") as f:
-    dataset = pickle.load(f)
+# import numpy as np
 
-import numpy as np
+# prediction = [1 for i in range(len(dataset))]
+# print(prediction)
 
-prediction = [1 for i in range(len(dataset))]
+# np.save('../results/task0_resnet34_cifar10.npy', prediction)
+# np.save('../results/task1_mobilenetv2_cifar10.npy', prediction)
+# np.save('../results/task2_resnet34_tinyimagenet.npy', prediction)
+# np.save('../results/task3_mobilenetv2_tinyimagenet.npy', prediction)
 
-np.save('../results/task0_resnet34_cifar10.npy', prediction)
-np.save('../results/task1_mobilenetv2_cifar10.npy', prediction)
-np.save('../results/task2_resnet34_tinyimagenet.npy', prediction)
-np.save('../results/task3_mobilenetv2_tinyimagenet.npy', prediction)
-
+# test = np.load('../results/task3_mobilenetv2_tinyimagenet.npy')
+# print(test)
